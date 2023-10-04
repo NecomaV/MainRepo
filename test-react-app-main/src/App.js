eb613689cba6a1db2a493040b755cd98808eae42
NecomaV <beken200225@gmail.com>
2023-10-05 01:08:55 +0600

    lets rtry

--- test-react-app-main/src/App.js
+++ test-react-app-main/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+
+import TestFile from '../../SubRepo/test-react-app-submodule/src/components/TestFile';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TestFile/>
+      he'll do it
     </div>
   );
 }
